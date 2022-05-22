94892ef3291fb2dddd98dccc48f5ab829998d80b
Nick Hanigan <13477700+nick-wireless@users.noreply.github.com>
2022-05-22 21:44:22 +0930

    New setup for ZSH with matching Dotbot Command
    Added path as well as switching to the new shell.

---
+++ setup_zsh.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+echo "\n<<< Starting ZSH Setup >>>\n"
+
+# Installation unnecessary; it is in the Brewfile.
+
+echo 'Enter sudo password to edit /etc/shells'
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+
+echo 'Enter user password to change login shell'
+chsh -s '/usr/local/bin/zsh'
