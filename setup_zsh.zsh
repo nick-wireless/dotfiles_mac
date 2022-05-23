a7e1992303f368e3b9f5e3a978300bcbe2f8917d
Nick Hanigan <13477700+nick-wireless@users.noreply.github.com>
2022-05-23 21:29:02 +0930

    Tidyups
    - Changing way to link sh to zsh, using dotbot's recommendation
    - Icing some of the main package installs with MacOS

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,21 +4,22 @@ echo "\n<<< Starting ZSH Setup >>>\n"
 # Installation unnecessary; it is in the Brewfile.
 
 if grep -Fxp '/usr/local/bin/zsh' '/etc/shells'; then
-  echo '/usr/local/bin/zsh already exists in /etc/shells'
+  echo 'Note: aborting copy as /usr/local/bin/zsh already exists in /etc/shells'
 else
   echo 'Enter sudo password to edit /etc/shells'
   echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 fi
 
-if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
-  echo '$SHELL is already /usr/local/bin/zsh'
-else
-  echo 'Enter user password to change login shell'
-  chsh -s '/usr/local/bin/zsh'
-fi
+# Holding code as seems to always run in Kitty - instead trying dotbot method (see install.conf.yaml)
+# if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+#   echo '$SHELL is already /usr/local/bin/zsh'
+# else
+#   echo 'Enter user password to change login shell'
+#   chsh -s '/usr/local/bin/zsh'
+# fi
 
 if sh --version | grep -q zsh; then 
-  echo '/private/var/select/sh already linked to /bin/zsh'
+  echo 'Note: /private/var/select/sh already linked to /bin/zsh'
 else
   echo 'Enter sudo password to simlink sh to zsh'
   # Not 100% but illustrative of the direction.
